bcebee7575a49747d6be06691665ed1530127f7a
kabuto <kemalmehmetmk7@gmail.com>
2020-10-24 20:28:54 +0100

    Added on change date listener Added Selected Date preview text Added Storage variables

--- app/src/main/java/com/example/calendarapptimeleft/MainActivity.java
+++ app/src/main/java/com/example/calendarapptimeleft/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.calendarapptimeleft;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.widget.CalendarView;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    CalendarView calendar;
+    TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendar = (CalendarView)findViewById(R.id.calendarView);
+        textview = (TextView)findViewById(R.id.textView);
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                textview.setText("Date is : " + dayOfMonth + " / " + (month+1) + " / " + year);
+            }
+        });
     }
 }
