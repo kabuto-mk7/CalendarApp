24d8db8b3051e411d77093aa4494879c47c2ddfe
kabuto-mk7 <kemalmehmetmk7@gmail.com>
2020-10-31 20:54:59 +0000

    Created DatabaseHelper class Imported SQLLITE dependencies Created simple database structure

---
+++ app/src/main/java/com/example/calendarapptimeleft/DatabaseHelper.java
@@ -0,0 +1,34 @@
+package applications.editablelistview;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "mylist.db";
+    public static final String TABLE_NAME = "timeleft_data";
+    public static final String COL1 = "ID";
+    public static final String COL2 = "EVENTNAME";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                " EVENTNAME TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP IF TABLE EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+
+
+}
