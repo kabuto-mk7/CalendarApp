cada97fab7366f0d91177b7dac71249c989c4781
kabuto-mk7 <kemalmehmetmk7@gmail.com>
2020-11-02 15:25:59 +0000

    added list view functionality to the view page added list adapter to convert SQL database to an android list view added error and non null checks to prevent errors

--- app/src/main/java/com/example/calendarapptimeleft/DayView.java
+++ app/src/main/java/com/example/calendarapptimeleft/DayView.java
@@ -2,16 +2,38 @@ package com.example.calendarapptimeleft;
 
 import android.os.Bundle;
 import android.widget.ListView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DayView extends AppCompatActivity {
+
+    DatabaseHelper myDB;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dayview_page);
 
-        Listview listview = (ListView) findViewById(R.id.listview)
+        ListView listView = (ListView) findViewById(R.id.listView);
+        myDB = new DatabaseHelper(this);
+
+        ArrayList<String> theList = new ArrayList<>();
+        Cursor data = myDB.getListContents();
+
+        if(data.getCount() == 0){
+            Toast.makeText(DayView.this,"Your task list is empty...",Toast.LENGTH_LONG).show();
+        }else{
+            theList.add(data.getString(1));
+            ListAdapter listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,theList);
+            listView.setAdapter(listAdapter);
+        }
     }
 }
