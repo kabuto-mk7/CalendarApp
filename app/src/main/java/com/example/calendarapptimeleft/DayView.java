d56b1de5709780ad5a4782e4d48bd88ae6644ff7
kabuto-mk7 <kemalmehmetmk7@gmail.com>
2020-11-05 15:22:14 +0000

    Fixed app crashing Fixed Event View error with cursor due to missing while statement

--- app/src/main/java/com/example/calendarapptimeleft/DayView.java
+++ app/src/main/java/com/example/calendarapptimeleft/DayView.java
@@ -24,13 +24,14 @@ public class DayView extends AppCompatActivity {
 
       ArrayList<String> theList = new ArrayList<>();
       Cursor data = myDB.getListContents();
-
       if(data.getCount() == 0){
-            Toast.makeText(DayView.this,"Your task list is empty...",Toast.LENGTH_LONG).show();
-        }else{
-            theList.add(data.getString(1));
-            ListAdapter listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,theList);
-            listView.setAdapter(listAdapter);
-        }
+          Toast.makeText(this, "There are no contents in this list!",Toast.LENGTH_LONG).show();
+      }else{
+          while(data.moveToNext()){
+              theList.add(data.getString(1));
+              ListAdapter listAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,theList);
+              listView.setAdapter(listAdapter);
+          }
+      }
     }
 }
